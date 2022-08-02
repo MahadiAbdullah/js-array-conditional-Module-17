c97d5d70da108faf12cf3614ffb52e84aed88f39
MahadiAbdullah <mahadi8603@gmail.com>
2022-08-02 14:41:56 +0600

    practice

---
+++ all-practice.js
@@ -0,0 +1,34 @@
+var fruits = ['Apple', 'Banana', 'Orange'];
+var position = fruits.indexOf('Banana');
+// console.log(position)
+
+// fruits['Banana'] = 'Mango';
+// console.log(fruits)
+var element = fruits;
+// console.log(element)
+// console.log(element)
+// 
+var element = fruits['Banana'];
+// console.log(element);
+fruits[1] = 'Mango'
+// console.log(fruits)
+fruits.pop();
+// console.log(fruits)
+fruits.push('Watermelon');
+// console.log(fruits)
+
+
+
+
+var fruits =['Apple', 'Banana', 'Orange'];
+var element = fruits.indexOf('Banana');
+console.log(element)
+
+var element = fruits['Banana'];
+fruits[1] = 'Mango';
+console.log(fruits)
+
+fruits.pop()
+console.log(fruits)
+fruits.push('Wetermelon');
+console.log(fruits)
